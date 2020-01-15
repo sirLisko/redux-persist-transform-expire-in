7b86e527c515c2193beb850c598ec62a58c5058e
Kacper  Rogowski <kacper.rogowski@cognifide.com>
2020-01-15 14:14:14 +0100

    Add support to change default value after expire

--- src/index.ts
+++ src/index.ts
@@ -9,35 +9,34 @@ import {
  * `redux-persist` transformer that reset the persisted redux state after a specific period of time.
  * @param {number} expireIn For how long the state is going to be preserved
  * @param {string} [expireKey="persistencyExpiration"] Key used by the localStorage
+ * @param {any} defaultValue Value to which state will be cleared to
  */
+
 const transformExpire = (
   expireIn: number,
-  expireKey: string = "persistencyExpiration"
+  expireKey: string = "persistencyExpiration",
+  defaultValue = {}
 ): Transform<any, any> => {
-  let expired = false;
   const storedExpiration = localStorage.getItem(expireKey);
+  let expired = false;
+
   if (storedExpiration) {
     const expiring = parseInt(storedExpiration);
     const now = new Date().getTime();
     expired = !isNaN(expiring) && now > expiring;
   }
+
   return createTransform(
-    (
-      inboundState: TransformIn<string, string>
-    ): TransformIn<string, string> => {
-      setTimeout(
-        (): void =>
-          localStorage.setItem(
-            expireKey,
-            (new Date().getTime() + expireIn).toString()
-          ),
-        0
-      );
+    (inboundState: TransformIn<string, string>) => {
+      setTimeout(() => {
+        const expireValue = (new Date().getTime() + expireIn).toString();
+        localStorage.setItem(expireKey, expireValue);
+      }, 0);
+
       return inboundState;
     },
-    (
-      outboundState: TransformOut<string, string>
-    ): TransformOut<string, string> | any => (expired ? {} : outboundState)
+    (outboundState: TransformOut<string, string>) =>
+      expired ? defaultValue : outboundState
   );
 };
 
